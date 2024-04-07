ae01acc841eb79c4c77cb947f45fc115547119b3
bhc <behicsakar@gmail.com>
2024-04-07 18:50:54 +0300

    Update Appwrite SDK configuration

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
-const sdk = require('node-appwrite');
+require("dotenv").config();
+const sdk = require("node-appwrite");
 
 let client = new sdk.Client();
 
 client
-  .setEndpoint(process.env.APPWRITE_ENDPOINT) // Your API Endpoint
-  .setProject(process.env.APPWRITE_PROJECT_ID) // Your project ID
-  .setKey(process.env.APPWRITE_API_KEY) // Your secret API key
-  .setSelfSigned() // Use only on dev mode with a self-signed SSL cert
-;
+  .setEndpoint(process.env.APP_ENDPOINT) // Your API Endpoint
+  .setProject(process.env.APP_PROJECT) // Your project ID
+  .setKey(process.env.APP_API_KEY) // Your secret API key
+  .setSelfSigned(); // Use only on dev mode with a self-signed SSL cert
 
 console.log(process.env);
