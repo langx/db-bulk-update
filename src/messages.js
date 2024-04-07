b9040a922d738fe0f7b4da139b7fe5402561909e
bhc <behicsakar@gmail.com>
2024-04-07 20:04:07 +0300

    Add messages.js file for fetching and updating documents

---
+++ src/messages.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const sdk = require("node-appwrite");
+
+let client = new sdk.Client();
+
+client
+  .setEndpoint(process.env.APP_ENDPOINT) // Your API Endpoint
+  .setProject(process.env.APP_PROJECT) // Your project ID
+  .setKey(process.env.APP_API_KEY) // Your secret API key
+  .setSelfSigned(); // Use only on dev mode with a self-signed SSL cert
+
+console.log("Client configured for messages");
+
+// List all messages
+let db = new sdk.Databases(client);
+
+console.log("Database instance created for messages");
+
+const LIMIT = 25;
+
+function updateDocument(docId, data) {
+  db.updateDocument(
+    process.env.APP_DATABASE,
+    process.env.MESSAGES_COLLECTION,
+    docId,
+    data
+  )
+    .then((response) => {
+      console.log(`Document ${docId} updated successfully`);
+    })
+    .catch((error) => {
+      console.error(`Error updating document ${docId}: ${error}`);
+    });
+}
+
+function listAllDocuments(offset) {
+  console.log(`Fetching documents with offset: ${offset}`);
+
+  db.listDocuments(process.env.APP_DATABASE, process.env.MESSAGES_COLLECTION, [
+    sdk.Query.orderAsc("$id"),
+    sdk.Query.offset(offset),
+    sdk.Query.limit(LIMIT),
+  ])
+    .then((response) => {
+      console.log(`Fetched ${response.documents.length} documents`);
+      console.log(`Total documents: ${response.total}`);
+
+      // Add your logic for messages here
+
+      if (offset + LIMIT < 300) {
+        // if (offset + LIMIT < response.total) {
+        listAllDocuments(offset + LIMIT);
+      } else {
+        console.log("Finished fetching all documents");
+      }
+    })
+    .catch((error) => {
+      console.error(`Error fetching documents: ${error}`);
+    });
+}
+
+module.exports = listAllDocuments;
