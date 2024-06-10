8f3b423b494a8cfae1673d74390248f674a40e71
xue <behicsakar@gmail.com>
2024-06-10 18:17:44 +0300

    Refactor code to process all users, messages, and user languages

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // index.js
 const processUsers = require("./src/users.js");
 const processMessages = require("./src/messages.js");
+const processUserLanguages = require("./src/userLanguages.js");
 
 async function processAll() {
-  processUsers(0);
-  processMessages(0);
+  // processUsers(0);
+  // processMessages(0);
+  processUserLanguages(0);
 }
 
 processAll();
