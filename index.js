92f2f7549e857317c8fa8d406ac96c668a701765
xue <behicsakar@gmail.com>
2024-06-18 00:59:39 +0300

    feat: Add setUsername.js to update usernames for all documents in the users collection

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const processMessages = require("./src/messages.js");
 const processUserLanguages = require("./src/userLanguages.js");
 const processUserBadges = require("./src/userBadges.js");
 const processCheckoutTotalAmount = require("./src/checkoutTotalAmount.js");
+const processSetUsername = require("./src/setUsername.js");
 
 async function processAll() {
   // processUsers(0);
   // processMessages(0);
   // processUserLanguages(0);
   // processUserBadges(0);
-  processCheckoutTotalAmount(0);
+  // processCheckoutTotalAmount(0);
+  // processSetUsername(0);
 }
 
 processAll();
