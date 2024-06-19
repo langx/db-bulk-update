3205e58c6e3d774af1538e28d016c62b10ab0a21
xue <behicsakar@gmail.com>
2024-06-19 04:54:28 +0300

    Refactor index.js to comment out unused imports and functions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // index.js
-const processUsers = require("./src/users.js");
-const processMessages = require("./src/messages.js");
-const processUserLanguages = require("./src/userLanguages.js");
-const processUserBadges = require("./src/userBadges.js");
-const processCheckoutTotalAmount = require("./src/checkoutTotalAmount.js");
-const processSetUsername = require("./src/setUsername.js");
+// const processUsers = require("./src/users.js");
+// const processMessages = require("./src/messages.js");
+// const processUserLanguages = require("./src/userLanguages.js");
+// const processUserBadges = require("./src/userBadges.js");
+// const processCheckoutTotalAmount = require("./src/checkoutTotalAmount.js");
+// const processSetUsername = require("./src/setUsername.js");
 
 async function processAll() {
   // processUsers(0);
